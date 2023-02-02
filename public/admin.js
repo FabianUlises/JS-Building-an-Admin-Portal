0e1706205d319bcfa45acca6471245365f5dc9e1
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2023-02-02 02:20:22 -0800

    Created some dom elements using js but going to do the rest in html file

--- public/admin.js
+++ public/admin.js
@@ -1,4 +1,16 @@
+const page = document.querySelector('#root');
+const booksContainer = document.createElement('div');
+booksContainer.classList.add('admin-books-container');
+page.append(booksContainer);
+// Get books from api
 const getBooks = async () => {
     let response = await fetch('http://127.0.0.1:3001/listBooks');
     let books = await response.json();
 }
+
+// Render books from api
+const renderBooks = (books) => {
+    books.forEach((book) => {
+    
+    });
+};
