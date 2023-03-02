4aafb86700d6105d3a1cb7e25343ef71549ec598
FabianUlises <ulisesfab.car@outlook.com>
2023-03-02 07:44:24 -0800

    Added update and delete route

--- books-api/controllers/bookRouters.js
+++ books-api/controllers/bookRouters.js
@@ -49,10 +49,26 @@ exports.createBook = async (req, res) => {
         });
     }
 };
+exports.updateBook = async (req, res) => {
+    try {
+        await Book.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json({
+            status: 'success',
+            message: 'Book was updated'
+        });
+    } catch(err) {
+        res.status(404).json({
+            status: 'fail',
+            message: `Unable to update book, err: ${err}`
+        });
+    }
+};
 exports.deleteBook = async (req, res) => {
     try {
+        console.log('deleting book');
         await Book.findByIdAndDelete(req.params.id);
-        res.status(204).json({
+        console.log('book deleted, sending json')
+        res.status(200).json({
             status: 'success',
             message: 'Book was deleted'
         });
@@ -60,6 +76,48 @@ exports.deleteBook = async (req, res) => {
         res.status(404).json({
             status: 'fail',
             message: `Unable to delete book, err: ${err}`
+        });
+    }
+};
+// Seed route
+exports.seedBooks = async(req, res) => {
+    try {
+        await Book.insertMany([{
+            "title": "The Shinobi Initiative",
+            "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+            "year": 2014,
+            "quantity": 10,
+            "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+          },
+          {
+            "title": "Tess the Wonder Dog",
+            "description": "The tale of a dog who gets super powers",
+            "year": 2007,
+            "quantity": 3,
+            "imageURL": "https://imgur.com/cEJmGKV.jpg"
+          },
+          {
+            "title": "The Annals of Arathrae",
+            "description": "This anthology tells the intertwined narratives of six fairy tales.",
+            "year": 2016,
+            "quantity": 8,
+            "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+          },
+          {
+            "title": "W∀RP",
+            "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+            "year": 2010,
+            "quantity": 4,
+            "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+          }]);
+          res.status(200).json({
+            status: 'success',
+            message: 'Seed successful'
+          });
+    } catch(err) {
+        res.status(400).json({
+            status: 'fail',
+            message: `Unable to seed books, err: ${err}`
         });
     }
 };
--- books-api/controllers/booksController.js
+++ books-api/controllers/booksController.js
@@ -3,8 +3,13 @@ const router = require('express').Router();
 // Bookroutes
 const bookRouter = require('./bookRouters');
 // Routes
+// Seed route
+router.get('/seed', bookRouter.seedBooks)
 router.route('/')
     .get(bookRouter.getAllBooks)
     .post(bookRouter.createBook)
-router.get('/:id', bookRouter.getBook);
+router.route('/:id')
+    .get(bookRouter.getBook)
+    .patch(bookRouter.updateBook)
+    .delete(bookRouter.deleteBook)
 module.exports = router;
