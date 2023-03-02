ec12c1e0e4ccc4f1e3ba6d90f76bc9b42bff89ac
FabianUlises <ulisesfab.car@outlook.com>
2023-03-02 07:51:39 -0800

    Installed cors

--- books-api/app.js
+++ books-api/app.js
@@ -2,9 +2,11 @@
 const book = require('./models/book');
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const booksController = require('./controllers/booksController');
 // Middleware
+app.use(cors());
 app.use(express.json());
 // Routes
 app.use('/api/v1/books', booksController);
